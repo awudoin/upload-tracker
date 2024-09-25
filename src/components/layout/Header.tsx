687895a829b2085613ff459a7da25937d7b16591
John Daniel (New Desktop) <awudoin@gmail.com>
2024-09-24 21:55:17 -0700

    Update z index for the header to place on top

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     const endContent = [<DarkModeToggleButton key='themeToggle' includeIcons />, ...headerContext.endContent];
 
     return (
-        <header className='bg-violet-600 text-slate-50 items-center px-3 py-2 fixed top-0 left-0 right-0 h-10'>
+        <header className='bg-violet-600 text-slate-50 items-center px-3 py-2 fixed top-0 left-0 right-0 h-10 z-30'>
             <div className='flex flex-row sm:hidden'>
                 <div className='flex flex-row gap-1 justify-start'>{headerContext.startContent.map((node) => node)}</div>
                 <div className='flex flex-row gap-1 grow justify-center'>{headerContext.middleContent.map((node) => node)}</div>
